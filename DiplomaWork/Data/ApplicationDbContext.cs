f69f2f5a13943f8bd45b8b9e8f9f85998e946cfc
MYNEJI <103054660+MYNEJI@users.noreply.github.com>
2025-01-25 20:00:17 +0600

    Добавьте файлы проекта.

---
+++ DiplomaWork/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DiplomaWork.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiplomaWork.Data
+{
+	public class ApplicationDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Category> Categories { get; set; }
+
+		//protected override void OnModelCreating(ModelBuilder modelBuilder)
+		//{
+		//	modelBuilder.Entity<Category>().HasData(
+		//		new Category { Id = 1, Name = "Foreighn Languages", DisplayOrder = 1 },
+		//		new Category { Id = 2, Name = "Musical courses", DisplayOrder = 2 },
+		//		new Category { Id = 3, Name = "Computer courses", DisplayOrder = 3 }
+		//		);
+		//}
+	}
+}
